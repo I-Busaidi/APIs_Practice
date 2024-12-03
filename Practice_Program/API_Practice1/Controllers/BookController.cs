b08b3b9b78633eaccfa76aa2812510d4dfa1d7ab
Ibrahim Al Busaidi <i.s.busaidi@gmail.com>
2024-12-03 16:53:21 +0400

    added methods to view books in books controller.

--- Practice_Program/API_Practice1/Controllers/BookController.cs
+++ Practice_Program/API_Practice1/Controllers/BookController.cs
@@ -1,6 +1,73 @@
-﻿namespace API_Practice1.Controllers
+﻿using API_Practice1.Models;
+using API_Practice1.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Practice1.Controllers
 {
-    public class BookController
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class BookController : ControllerBase
     {
+        private readonly IBookService _bookService;
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet("GetAllBooks")]
+        public IActionResult GetAllBooks()
+        {
+            try
+            {
+                var books = _bookService.GetAllBooks();
+                return Ok(books);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("GetBookById/{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            try
+            {
+                var book = _bookService.GetBookById(id);
+                return Ok(book);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("GetBookByName/{name}")]
+        public IActionResult GetBookByName(string name)
+        {
+            try
+            {
+                var books = _bookService.GetBookByName(name);
+                return Ok(books);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult AddBook([FromBody] Book book)
+        {
+            try
+            {
+                string newBookName = _bookService.AddBook(book);
+                return Created(string.Empty, newBookName);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
